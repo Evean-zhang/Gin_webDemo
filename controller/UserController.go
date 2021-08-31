5ba12c67c2fe1a3de0ed2204870689e6098b19cf
xianze_zhang <xianze_zhang@kingdee.com>
2021-08-31 20:58:03 +0800

    登录功能（注册功能一直报手机号码为11位）

--- controller/UserController.go
+++ controller/UserController.go
@@ -6,6 +6,7 @@ import (
 	"Gin_webDemo/util"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
 	"log"
 	"net/http"
 )
@@ -40,6 +41,7 @@ func Register(context *gin.Context) {
 		name = util.RandomString(10)
 	}
 	log.Panicln(name, phone, password)
+
 	//判断手机号是否存在
 	if PhoneExist(DB, phone) {
 		context.JSON(http.StatusUnprocessableEntity, gin.H{
@@ -48,17 +50,78 @@ func Register(context *gin.Context) {
 		})
 		return
 	}
-	//创建用户
+	//创建用户   密码加密
+	hasedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"code":    500,
+			"message": "加密码错误",
+		})
+	}
 	newUser := model.User{
 		Name:     name,
 		Phone:    phone,
-		Password: password,
+		Password: string(hasedPassword),
 	}
 	DB.Create(&newUser)
 
 	//返回结果
 	context.JSON(http.StatusOK, gin.H{
+		"code":    200,
 		"message": "注册成功",
+	})
+}
+
+func Login(context *gin.Context) {
+	DB := common.GetDB()
+
+	//获取参数
+	phone := context.PostForm("phone")
+	password := context.PostForm("password")
+
+	//数据验证
+	if len(phone) != 11 {
+		context.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code":    422,
+			"message": "手机号必须为11位",
+		})
+		return
+	}
+	if len(password) < 6 {
+		context.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code":    422,
+			"message": "密码至少为6位",
+		})
+		return
+	}
+
+	//判断手机号是否存在
+	var user model.User
+	DB.Where("phone = ?", phone).First(&user)
+	if len(password) == 0 {
+		context.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code":    422,
+			"message": "用户不存在",
+		})
+		return
+	}
+	//判断密码是否正确
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"code":    400,
+			"message": "密码错误",
+		})
+		return
+	}
+
+	//发放Token给前端
+	token := "11"
+
+	//返回结果
+	context.JSON(http.StatusOK, gin.H{
+		"code":    200,
+		"data":    gin.H{"token": token},
+		"message": "登陆成功",
 	})
 }
 
--- util/util.go
+++ util/util.go
@@ -6,7 +6,7 @@ import (
 )
 
 func RandomString(n int) string {
-	var letters = []byte("asdawdadasfadsfadfsadf")
+	var letters = []byte("asdfghjklzxcvbnmqwertyuiopASDFGHJKLZXCVBNMQWERTYUIOP")
 	result := make([]byte, n)
 	rand.Seed(time.Now().Unix())
 	for i := range result {
